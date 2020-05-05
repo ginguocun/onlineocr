0861143929331d174936636afcbf1907fdeec7f3
金国群 <guoqun.jin@hotmail.com>
2020-05-05 12:05:04 +0800

    add tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,154 @@
+import os
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from app.models import ImageUpload
+
+
+user_data = {
+    'username': 'test',
+    'password': 'TestPass2020',
+    'email': 'test@hotmail.com'
+}
+
+login_data = {
+    'username': user_data['username'],
+    'password': user_data['password'],
+}
+
+image_data = {
+    'path': 'test/test01.png',
+    'image': os.path.join(settings.MEDIA_ROOT, 'test/test01.png'),
+    'file_name': 'test01.png',
+    'letters': [
+        'Django', 'makes', 'it', 'easier', 'to', 'build', 'better', 'Web',
+        'apps', 'more', 'quickly', 'and', 'with', 'less', 'code.'],
+    'count': 15,
+    'created_by': None
+}
+
+url_data = {
+    'token_obtain_url': '/api/token_obtain_pair/',
+    'token_refresh_url': '/api/token_refresh/',
+    'ocr_url': '/api/ocr/',
+    'history_url': '/api/history/'
+}
+
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user_data = user_data
+
+    def test_create_user(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_user(**self.user_data)
+        self.assertEqual(user.email, self.user_data['email'])
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_login(self):
+        is_login = self.client.login(username=self.user_data['username'], password=self.user_data['password'])
+        self.assertFalse(is_login, False)
+        user_model = get_user_model()
+        user_model.objects.create_user(**self.user_data)
+        is_login = self.client.login(username=self.user_data['username'], password=self.user_data['password'])
+        self.assertTrue(is_login, True)
+
+
+class ImageUploadModelTest(TestCase):
+
+    def setUp(self):
+        self.image_data = image_data
+        self.user_data = user_data
+
+    def test_create_1(self):
+        image_upload_record_1 = ImageUpload.objects.create(image=self.image_data['path'])
+        self.assertEqual(image_upload_record_1.file_name, self.image_data['file_name'])
+        self.assertEqual(image_upload_record_1.letters, self.image_data['letters'])
+        self.assertEqual(image_upload_record_1.count, self.image_data['count'])
+        self.assertEqual(image_upload_record_1.created_by, self.image_data['created_by'])
+
+    def test_create_2(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_user(**self.user_data)
+        user_id = user.id
+        image_upload_record_2 = ImageUpload.objects.create(image=self.image_data['path'], created_by_id=user_id)
+        self.assertEqual(image_upload_record_2.file_name, self.image_data['file_name'])
+        self.assertEqual(image_upload_record_2.letters, self.image_data['letters'])
+        self.assertEqual(image_upload_record_2.count, self.image_data['count'])
+        self.assertEqual(image_upload_record_2.created_by, user)
+
+
+class TokenObtainTest(TestCase):
+
+    def setUp(self):
+        self.user_data = user_data
+        self.login_data = login_data
+        self.url_data = url_data
+
+    def test_token_obtain(self):
+        response = self.client.post(path=self.url_data['token_obtain_url'], data=self.login_data)
+        self.assertEqual(response.status_code, 401)
+        # create a user
+        user_model = get_user_model()
+        user_model.objects.create_user(**self.user_data)
+        response = self.client.post(path=self.url_data['token_obtain_url'], data=self.login_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_token_refresh(self):
+        # create a user
+        user_model = get_user_model()
+        user_model.objects.create_user(**self.user_data)
+        # get the refresh token
+        token_obtain = self.client.post(path=self.url_data['token_obtain_url'], data=self.login_data)
+        refresh = token_obtain.data['refresh']
+        # obtain the access token by the refresh token
+        response = self.client.post(path=self.url_data['token_refresh_url'], data={'refresh': refresh})
+        self.assertEqual(response.status_code, 200)
+
+
+class OcrAPIViewTest(TestCase):
+
+    def setUp(self):
+        self.user_data = user_data
+        self.login_data = login_data
+        self.image_data = image_data
+        self.url_data = url_data
+
+    def test_ocr_api_1(self):
+        response = self.client.post(
+            path=self.url_data['ocr_url'],
+            data={'image': open(self.image_data['image'], 'rb')}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('content', response.data)
+        history = self.client.get(path=self.url_data['history_url'])
+        self.assertEqual(history.status_code, 401)
+
+    def test_ocr_api_2(self):
+        # create a user
+        user_model = get_user_model()
+        user = user_model.objects.create_user(**self.user_data)
+        user_id = user.id
+        # user login
+        self.client.login(**self.login_data)
+        # upload an image
+        ocr = self.client.post(
+            path=self.url_data['ocr_url'],
+            data={'image': open(self.image_data['image'], 'rb')}
+        )
+        self.assertEqual(ocr.status_code, 200)
+        self.assertIn('content', ocr.data)
+        # check history
+        history = self.client.get(path=self.url_data['history_url'])
+        self.assertEqual(history.status_code, 200)
+        self.assertIn('count', history.data)
+        self.assertIn('results', history.data)
+        self.assertEqual(len(history.data['results']), 1)
+        self.assertEqual(history.data['results'][0]['created_by'], user_id)
+        self.assertEqual(history.data['results'][0]['file_name'], self.image_data['file_name'])
+
